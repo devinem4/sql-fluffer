c9a91f62133b128fefe6aa5d0b2e092ede3f5709
matt devine <mddevine@gmail.com>
2022-02-22 22:21:03 -0500

    Add jinja templates

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from datetime import datetime
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import sqlfluff
 
 app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/")
 def root():
     return { "time": datetime.now() }
+
+@app.get("/fluff", response_class=HTMLResponse)
+async def fluff(request: Request):
+    return templates.TemplateResponse("fluff.html", {"request": request})
+
