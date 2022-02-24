db79a19564bef72e9e30dd790194498486e946fb
matt devine <mddevine@gmail.com>
2022-02-23 22:45:42 -0500

    :heavy_plus_sign: Add .sqlfluff file

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -8,20 +7,29 @@ import sqlfluff
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
+sqlfluff_config = ".sqlfluff"
 
 class Sql(BaseModel):
     sql: str
 
-@app.get("/")
-def root():
-    return { "time": datetime.now() }
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("fluff.html", {"request": request})
 
 
-@app.post("/lint/")
-async def lint(sql: Sql):
+@app.post("/fluff/")
+async def fluff(sql: Sql):
     try:
-        fixed = sqlfluff.fix(sql.sql, dialect="ansi", fix_even_unparsable=True)
-        lint = sqlfluff.lint(fixed, dialect="ansi")
+        fixed = sqlfluff.fix(
+            sql.sql,
+            dialect="ansi",
+            config_path=sqlfluff_config,
+        )
+        lint = sqlfluff.lint(
+            fixed,
+            dialect="ansi",
+            config_path=sqlfluff_config,
+        )
     except:
         return {
             "original": sql.sql,
@@ -32,9 +40,3 @@ async def lint(sql: Sql):
       "fixed": fixed,
       "lint": lint
     }
-
-
-@app.get("/fluff/", response_class=HTMLResponse)
-async def fluff(request: Request):
-    return templates.TemplateResponse("fluff.html", {"request": request})
-
