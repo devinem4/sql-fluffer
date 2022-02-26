0b36734af323a3f639e9912b49d3f2e623573cb8
matt devine <mddevine@gmail.com>
2022-02-25 22:56:05 -0500

    :radio_button: Add a button to fluff sql

--- app/js/sql_fluffer.js
+++ app/js/sql_fluffer.js
@@ -12,19 +12,41 @@ const Editor = ({ placeHolder, onChange, value, readOnly }) => {
 
 const App = () => {
   const [sql, setSql] = React.useState("");
+  const [fixedSql, setFixedSql] = React.useState("");
+
+  const fluffSql = () => {
+    const endpoint = "https://sql-fluffer.herokuapp.com/fluff/";
+    // const endpoint = "http://localhost:8000/fluff/";
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ sql: sql }),
+    };
+    fetch(endpoint, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setFixedSql(data["fixed"]);
+      });
+  };
+
   return (
-    <div className="container">
-      <Editor
-        placeHolder="Type your code here..."
-        onChange={(e) => setSql(e.target.value)}
-        readOnly={false}
-      />
-      <Editor
-        class="fixed"
-        placeHolder="Fixed SQL goes here"
-        value={sql}
-        readOnly={true}
-      ></Editor>
+    <div className="app">
+      <div className="container">
+        <Editor
+          placeHolder="Type your code here..."
+          onChange={(e) => setSql(e.target.value)}
+          readOnly={false}
+        />
+        <Editor
+          class="fixed"
+          placeHolder="Fixed SQL goes here"
+          value={fixedSql}
+          readOnly={true}
+        ></Editor>
+      </div>
+      <hr />
+      <button onClick={fluffSql}>fluff it!</button>
     </div>
   );
 };
